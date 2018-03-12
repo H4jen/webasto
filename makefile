0970746c523cd272a8554adda82255b0afdce55c
H4j3n <aterweggo2@gmail.com>
2018-03-12 23:23:09 +0000

    some changes to support 8e1 serial

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 ARDUINO_DIR  = /usr/share/arduino
 BOARD_TAG    = uno
 ARDUINO_PORT = /dev/ttyACM0
-ARDUINO_LIBS = LiquidCrystal Wire SoftwareSerial
+ARDUINO_LIBS = LiquidCrystal Wire CustomSoftwareSerial
 
 
 include /usr/share/arduino/Arduino.mk
--- webasto.cpp
+++ webasto.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 //#include <HardwareSerial.h>
-#include <SoftwareSerial.h>
+#include <CustomSoftwareSerial.h>
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
@@ -17,37 +17,52 @@
 #define D6_pin  6
 #define D7_pin  7
  
-#define INVERT_SIGNAL true
+#define INVERT_SIGNAL false
 
 #define BLINK_DELAY_MS 2400
-#define BAUD_RATE 300
-
+//Baud rate of software serial (interface to Heater)
+#define BAUD_RATE 2400
 #define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+
 #ifdef DEBUG    //Macros are usually in all capital letters.
   #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
+  #define DPRINTHEX(...) Serial.print(__VA_ARGS__,HEX)
   #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
+  #define DPRINTLNHEX(...) Serial.println(__VA_ARGS__,HEX);
 #else
   #define DPRINT(...)     //now defines a blank line
   #define DPRINTLN(...)   //now defines a blank line
+  #define DPRINTHEX(...)
+  #define DPRINTLNHEX(...)
 #endif
 
+//byte message[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+//byte message2[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 
 //#define setbit(port, bit) (port) |= (1 << (bit))
 //#define clearbit(port, bit) (port) &= ~(1 << (bit))
  
 //can only be pins 8-13 because prtmapping is hardcoded to PORTB 
-int8_t rxPin=10;
+uint8_t rxPin=10;
 uint8_t txPin=11;
 
 // set up a new serial port
-SoftwareSerial mySerial =  SoftwareSerial(rxPin,txPin,INVERT_SIGNAL);
+CustomSoftwareSerial* mySerial;
+//=  SoftwareSerial(rxPin,txPin,INVERT_SIGNAL);
+
 LiquidCrystal_I2C	lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 
 void init_board() {
  
  //call arduino init()
  init();
- mySerial.begin(BAUD_RATE);
+ mySerial = new CustomSoftwareSerial(rxPin, txPin); // rx, tx
+ mySerial->begin(2400, CSERIAL_8E1);         // Baud rate: 9600, configuration: CSERIAL_8N1
+ //mySerial->write("Test message");            // Write testing data
+
+//mySerial.begin(BAUD_RATE);
+
+
 #ifdef DEBUG
  Serial.begin(9600);
  DPRINTLN("Hello world");
@@ -71,12 +86,18 @@ int main (void)
 {
 //Init arduino
 init_board();
-
+//int FFcounter = 0;
 // main loop
 while(1) {
-  delay(BLINK_DELAY_MS);
-  mySerial.println("Hello World - SoftwareSerial");
-  DPRINTLN("bajs bajs");
-
+  int c = mySerial->read();
+  if(c != -1) { 
+	if(c == 0xF4){
+	  DPRINTLN(' ');
+	  DPRINTHEX(c);
+	}
+	else { DPRINTHEX(c);}
+	DPRINT(' ');
+	
+  }
  }
 }
