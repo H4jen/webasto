33c0eb73ac0aa87d0435c18e27c682494e84c605
H4j3n <aterweggo2@gmail.com>
2018-03-15 12:39:20 +0000

    removed message.h and message.cpp and added wbus.h and wbus.cpp

---
+++ wbus.h
@@ -0,0 +1,22 @@
+#ifndef WBUS_H
+#define WBUS_H
+
+#include "webasto.h"
+
+extern const int TX_MESSAGE_INIT_1[];
+
+struct rx_message
+{
+  int header=0;
+  int length=0;
+  int data[MESSAGE_BUFFER_SIZE];
+  int nr_data_read=0;
+  int checksum=0; 
+  bool valid_message = false;
+};
+
+//put your function headers here
+void parse_message();
+
+void sendTXmessage(const int msg[]);
+#endif
